f592d338ac7265186438ede4892ccd26c1a612e0
liyutian <liyutian>
2021-09-23 17:11:38 +0800

    测试文档提交

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int i=1;
+        double j=0;
+        long f;
+        switch(i){
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+        switch(i){
+            case 0:
+                break;
+            case 1:
+                break;
+            default:
+                break;
+        }
+        if(i<0){
+            if(i<-1){
+            } else{ }
+        } else if(i>0){
+            if (i>2){
+            } else if (i==2) {
+            } else if (i>1) {
+            } else { }
+        } else{
+            if(j!=0){
+            } else{ }
+        }
     }
 }
